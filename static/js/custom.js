ce7c397e42d259b37a49838050cf25bda011b972
yu xiaoyang <xiaoyang.yu@saicocch.com>
2014-07-01 11:49:07 +0800

    add create tv show

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,40 +1,60 @@
-if ($('#summernote').length) {
-      $('#summernote').summernote({
+if ($('.summernote').length) {
+      $('.summernote').summernote({
         height: 600,
         focus: true,
         toolbar: [['style', ['style']], ['style', ['bold', 'italic', 'underline', 'clear']], ['fontsize', ['fontsize']], ['color', ['color']], ['para', ['ul', 'ol', 'paragraph']], ['height', ['height']], ['insert', ['picture', 'link']], ['table', ['table']], ['fullscreen', ['fullscreen']]]
       });
 }
-/*
-# =============================================================================
-#   DataTables
-# =============================================================================
-*/
 
-$("#dataTable1").dataTable({
-  "sPaginationType": "full_numbers",
-  aoColumnDefs: [
-    {
-      bSortable: false,
-      aTargets: [0, -1]
-    }
-  ]
-});
-$('.table').each(function() {
-  return $(".table #checkAll").click(function() {
-    if ($(".table #checkAll").is(":checked")) {
-      return $(".table input[type=checkbox]").each(function() {
-        return $(this).prop("checked", true);
-      });
-    } else {
-      return $(".table input[type=checkbox]").each(function() {
-        return $(this).prop("checked", false);
-      });
-    }
-  });
-});
 
 $(function(){
+	/*
+	# =============================================================================
+	#   DataTables
+	# =============================================================================
+	*/
+
+	$("#dataTable1").dataTable({
+	  "sPaginationType": "full_numbers",
+	  aoColumnDefs: [
+	    {
+	      bSortable: false,
+	      aTargets: [0, -1]
+	    }
+	  ]
+	});
+	$('.table').each(function() {
+	  return $(".table #checkAll").click(function() {
+	    if ($(".table #checkAll").is(":checked")) {
+	      return $(".table input[type=checkbox]").each(function() {
+	        return $(this).prop("checked", true);
+	      });
+	    } else {
+	      return $(".table input[type=checkbox]").each(function() {
+	        return $(this).prop("checked", false);
+	      });
+	    }
+	  });
+	});
+	/*
+    # =============================================================================
+    #   File upload buttons
+    # =============================================================================
+    */
+
+    $('.fileupload').fileupload();
+    /*
+    # =============================================================================
+    #   Form wizard
+    # =============================================================================
+    */
+    
+    
+    /*
+    # =============================================================================
+    #   tv save
+    # =============================================================================
+    */
 	$(".btn-save").click(function(){
 		var id = $(this).attr("id");
 		var desc = $(".note-editable").html();
