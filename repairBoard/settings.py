42d8f6ef151b8c39a850b915e0e06cd7f8416a18
yu xiaoyang <xiaoyang.yu@saicocch.com>
2014-07-14 17:19:38 +0800

    屏幕变小时sidebar隐藏

--- repairBoard/settings.py
+++ repairBoard/settings.py
@@ -18,7 +18,16 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': "vDoUHbGmGoBlHuxTxOWL",                      # Or path to database file if using sqlite3.
+#         'USER': 'jF8BtOpxqGx4YR3hHD3PUBj4',                      # Not used with sqlite3.
+#         'PASSWORD': '4SY32nFpTmzeIvaGNFYPHjkv1V8xdcxL',                  # Not used with sqlite3.
+#         'HOST': 'sqld.duapp.com',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '4050',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
