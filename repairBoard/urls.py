c8a65d14cc4ec5fc419decc39f2d9a0061f29e17
yu xiaoyang <xiaoyang.yu@saicocch.com>
2014-07-23 18:10:39 +0800

    更新bae发布方法

--- repairBoard/urls.py
+++ repairBoard/urls.py
@@ -27,5 +27,5 @@ urlpatterns = patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG}),
+    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG}),
 )
